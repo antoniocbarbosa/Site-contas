9b5cc1c69d38a3e4f70a89ab8f657bb24e41bd87
Antônio Carlos Ribeiro Barbosa <antoniocarlos260@outlook.com>
2021-06-28 13:27:17 -0300

    Close #8

---
+++ site/_bd/edit_varied.php
@@ -0,0 +1,68 @@
+<?php
+
+if (isset($_POST['data_conta']))
+{
+
+    session_start();
+
+    require_once 'connection.php';
+    require_once '../functions.php';
+
+    $conn = getConnection($_SESSION['pagina']);
+
+    if ($conn)
+    {
+
+        //Registrar contas variadas
+        if ($_SESSION['opc'] == 1)
+        {
+
+            if (empty($_POST['data_conta']) || empty($_POST['nome']) || empty($_POST['valor']))
+            {
+                $_SESSION['erro'] = 'Não pode deixar nenhum campo obrigatório em branco. Os campos obrigatórios tem um asterisco * na frente do nome.';
+                header('Location: ' . $_SESSION['pagina']);
+                exit;
+            }
+
+            $sql = ('INSERT INTO variadas (data_conta, nome, valor, situacao, observacao)
+            VALUES
+            (:data_conta, :nome, :valor, :situacao, :observacao)');
+
+            $stmt = $conn -> prepare($sql);
+
+            $data_conta = $_POST['data_conta'];
+            $nome = addslashes(trim(preg_replace('/ + /', ' ', $_POST['nome'])));
+            $valor = (float) $_POST['valor'];
+            $situacao = $_POST['situacao'];
+            $observacao = addslashes(isset($_POST['observacao']) ? trim(preg_replace('/ + /', ' ', $_POST['observacao'])) : null);
+
+            $stmt -> bindParam(':data_conta', $data_conta);
+            $stmt -> bindParam(':nome', $nome);
+            $stmt -> bindParam(':valor', $valor);
+            $stmt -> bindParam(':situacao', $situacao);
+            $stmt -> bindParam(':observacao', $observacao);
+
+            execQuery($stmt);
+
+            header('Location: ' . $_SESSION['pagina']);
+            exit;
+
+        }
+        elseif ($_SESSION['opc'] == 2)
+        {
+
+        }
+
+    }
+    elseif (!$conn)
+    {
+        header('Location: ' . $_SESSION['pagina']);
+        exit;
+    }
+
+}
+else
+{
+    header('Location: ../index.php');
+    exit;
+}
--- site/register/varied.php
+++ site/register/varied.php
@@ -5,7 +5,7 @@
     
     <div class="register">
 
-        <form action="" method="POST">
+        <form action="../_bd/edit_varied.php" method="POST">
             <label for="data_conta">Data da conta*:</label>
             <input type="date" name="data_conta" id="data_conta" min="2000-01-01"><br>
             <label for="nome">Nome da conta*:</label>
@@ -15,14 +15,24 @@
             <fieldset><legend>Situação:</legend>
                 <input type="radio" name="situacao" id="p" value="P">
                 <label for="p">Conta paga</label><br>
-                <input type="radio" name="situacao" id="np" value="NP">
+                <input type="radio" name="situacao" id="np" value="NP" checked>
                 <label for="np">Conta não paga</label>
             </fieldset>
             <label for="observacao">Observação:</label><br>
             <textarea name="observacao" id="observacao" cols="60" rows="6" maxlength="200" placeholder="Alguma característica da conta." style="resize: none;"></textarea><br>
             <input type="submit" value="Registrar" id="button">
             <a href="register.php"><input type="button" value="Voltar" id="button"></a>
+
+            <?php
+                $_SESSION['opc'] = 1;
+                $_SESSION['pagina'] = '../register/varied.php';
+            ?>
         </form>
+
+        <?php
+            require_once '../functions.php';
+            checarCadastro();
+        ?>
 
     </div>
 
