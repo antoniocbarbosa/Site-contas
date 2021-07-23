87c9a49acb2ecdbe5ae25af0da3803a39484ac4d
Antônio Barbosa <antoniocarlos260@outlook.com>
2021-07-23 15:41:32 -0300

    Close #16

--- site/_bd/edit_varied.php
+++ site/_bd/edit_varied.php
@@ -8,55 +8,56 @@
         require_once 'connection.php';
         require_once '../functions.php';
 
-        $conn = getConnection($_SESSION['pagina']);
+        $conn = getConnection();
+        $page = isset($_POST['page']) ? $_POST['page'] : '../register/varied.php';
 
         if ($conn)
         {
-
-            //Registrar contas variadas
-            if ($_SESSION['opc'] == 1)
+            if (empty($_POST['data_conta']) || empty($_POST['nome']) || empty($_POST['valor']))
             {
-
-                if (empty($_POST['data_conta']) || empty($_POST['nome']) || empty($_POST['valor']))
-                {
-                    $_SESSION['erro'] = 'Não pode deixar nenhum campo obrigatório em branco. Os campos obrigatórios tem um asterisco * na frente do nome.';
-                    header('Location: ' . $_SESSION['pagina']);
-                    exit;
-                }
+                $_SESSION['error'] = 'Não pode deixar nenhum campo obrigatório em branco. Os campos obrigatórios tem um asterisco * na frente do nome.';
+                header('Location: ' . $page);
+                exit;
+            }
+            
+            if ($_SESSION['opc'] == 1) //Registrar contas variadas
+            {
+                $action = 'register';
 
                 $sql = ('INSERT INTO variadas (data_conta, nome, valor, situacao, observacao)
                 VALUES
                 (:data_conta, :nome, :valor, :situacao, :observacao)');
-
-                $stmt = $conn -> prepare($sql);
-
-                $data_conta = $_POST['data_conta'];
-                $nome = addslashes(trim(preg_replace('/ + /', ' ', $_POST['nome'])));
-                $valor = (float) $_POST['valor'];
-                $situacao = $_POST['situacao'];
-                $observacao = addslashes(isset($_POST['observacao']) ? trim(preg_replace('/ + /', ' ', $_POST['observacao'])) : null);
-
-                $stmt -> bindParam(':data_conta', $data_conta);
-                $stmt -> bindParam(':nome', $nome);
-                $stmt -> bindParam(':valor', $valor);
-                $stmt -> bindParam(':situacao', $situacao);
-                $stmt -> bindParam(':observacao', $observacao);
-
-                execQuery($stmt, 'cadastro');
-
-                header('Location: ' . $_SESSION['pagina']);
-                exit;
-
             }
-            elseif ($_SESSION['opc'] == 2)
+            elseif ($_SESSION['opc'] == 2) //Atualizar contas variadas
             {
+                $action = 'update';
+                $id = $_POST['id'];
 
+                $sql = ("UPDATE variadas SET data_conta = :data_conta, nome = :nome, valor = :valor, situacao = :situacao, observacao = :observacao WHERE id = '{$id}'");
             }
 
+            $stmt = $conn -> prepare($sql);
+
+            $data_conta = $_POST['data_conta'];
+            $nome = addslashes(trim(preg_replace('/ + /', ' ', $_POST['nome'])));
+            $valor = (float) $_POST['valor'];
+            $situacao = $_POST['situacao'];
+            $observacao = addslashes(isset($_POSTeee['observacao']) ? trim(preg_replace('/ + /', ' ', $_POST['observacao'])) : null);
+
+            $stmt -> bindParam(':data_conta', $data_conta);
+            $stmt -> bindParam(':nome', $nome);
+            $stmt -> bindParam(':valor', $valor);
+            $stmt -> bindParam(':situacao', $situacao);
+            $stmt -> bindParam(':observacao', $observacao);
+
+            execQuery($stmt, $action);
+
+            header('Location: ' . $page);
+            exit;
         }
         else
         {
-            header('Location: ' . $_SESSION['pagina']);
+            header('Location: ' . $page);
             exit;
         }
 
--- site/register/varied.php
+++ site/register/varied.php
@@ -1,5 +1,26 @@
-<?php session_start(); require_once '../header.php'; ?>
-    <title>Registrar variadas</title>
+<?php 
+
+    session_start();
+    
+    if (isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['opc']) 
+        && $_GET['opc'] == 2)
+    {
+        require_once '../functions.php';
+    
+        $id = isset($_GET['id']) ? $_GET['id'] : null;
+        $res = buscarDadosConta('variadas', 'id', ':id', $id, 'varied');
+
+        require_once '../header.php';
+        echo '<title>Atualizar variada</title>';
+    }
+    else
+    {
+        require_once '../header.php';
+        echo '<title>Registar variada</title>';
+    }
+
+?>
+    <script type="text/javascript" src="../_js/previousPage.js";></script>
 </head>
 <body>
     
@@ -7,31 +28,39 @@
 
         <form action="../_bd/edit_varied.php" method="POST">
             <label for="data_conta">Data da conta*:</label>
-            <input type="date" name="data_conta" id="data_conta" min="2000-01-01"><br>
+            <input type="date" name="data_conta" id="data_conta" min="2000-01-01" value="<?php if(isset($res)) {echo $res['data_conta'];} ?>"><br>
+            <input type="number" name="id" id="id" value="<?php if (isset($res)) {echo $id;} ?>" hidden>
             <label for="nome">Nome da conta*:</label>
-            <input type="text" name="nome" id="nome" size="50" maxlength="50" placeholder="Nome do local ou da pessoa com quem foi feito a conta."><br>
+            <input type="text" name="nome" id="nome" size="50" maxlength="50" placeholder="Nome do local ou da pessoa com quem foi feito a conta." value="<?php if(isset($res)) {echo $res['nome'];} ?>"><br>
             <label for="valor">Valor*:</label>
-            <input type="number" name="valor" id="valor" min="1" max="20000" step="0.01" placeholder="R$ 00.00"><br>
+            <input type="number" name="valor" id="valor" min="1" max="20000" step="0.01" placeholder="R$ 00.00" value="<?php if(isset($res)) {echo $res['valor'];} ?>"><br>
             <fieldset><legend>Situação:</legend>
-                <input type="radio" name="situacao" id="p" value="P">
+                <input type="radio" name="situacao" id="p" value="P" <?php isset($res) && !empty($res) && $res['situacao'] == 'P' ? print 'checked' : ''?>>
                 <label for="p">Conta paga</label><br>
-                <input type="radio" name="situacao" id="np" value="NP" checked>
+                <input type="radio" name="situacao" id="np" value="NP" <?php isset($res) && !empty($res) && $res['situacao'] != 'P' ? print 'checked' : (!isset($res) ? print 'checked' : '')?>> 
                 <label for="np">Conta não paga</label>
             </fieldset>
             <label for="observacao">Observação:</label><br>
-            <textarea name="observacao" id="observacao" cols="60" rows="6" maxlength="200" placeholder="Alguma característica da conta." style="resize: none;"></textarea><br>
-            <input type="submit" value="Registrar" id="button">
-            <a href="register.php"><input type="button" value="Voltar" id="button"></a>
+            <textarea name="observacao" id="observacao" cols="60" rows="6" maxlength="200" placeholder="Alguma característica da conta." style="resize: none;"><?php if(isset($res) && !empty($res)) {echo $res['observacao'];} ?></textarea><br>
+            <input type="text" name="page" id="page" hidden>
+            <input type="submit" onclick="setPage()" id="button" value="<?php if (isset($_GET['id']) &&  !empty($_GET['id']) && isset($_GET['opc']) && $_GET['opc'] == 2) {echo 'Atualizar';} else {echo 'Registrar';} ?>">
+            <a href="<?php if(isset($res)) {echo '../search.php';} else {echo 'register.php';} ?>"><input type="button" id="button" value="Voltar"></a>
 
             <?php
-                $_SESSION['opc'] = 1;
-                $_SESSION['pagina'] = '../register/varied.php';
+                if (isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['opc']) && $_GET['opc'] == 2)
+                {
+                    $_SESSION['opc'] = 2;
+                }
+                else
+                {
+                    $_SESSION['opc'] = 1;
+                }
             ?>
         </form>
 
         <?php
             require_once '../functions.php';
-            checarCadastro();
+            checarAcao();
         ?>
 
     </div>
